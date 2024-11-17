cdcbd2849337a714962b136b64a1310eaf818528
Shobhita <aggrawalshobhi24@gmail.com>
2024-11-17 11:41:20 -0800

    changing to updated code

--- uber_transport_simulation/backend/rides/serializers.py
+++ uber_transport_simulation/backend/rides/serializers.py
@@ -1,37 +1,24 @@
 from rest_framework import serializers
-from rides.models import Ride
-from django.utils import timezone
-from django.contrib.auth import get_user_model
+from .models import Ride
+from django.utils.timezone import now
 
-User = get_user_model()
 
 class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
         fields = [
-            'ride_id',
-            'customer',
-            'driver',
-            'pickup_location',
-            'dropoff_location',
-            'pickup_datetime',
-            'dropoff_datetime',
-            'distance',
-            'status',
+            'ride_id', 'customer', 'pickup_location', 'dropoff_location',
+            'pickup_datetime', 'distance', 'duration', 'estimated_price', 'status'
         ]
-        read_only_fields = ['ride_id', 'customer', 'status', 'dropoff_datetime']  # Ensure auto-managed fields are not editable by the user
+        read_only_fields = ['ride_id', 'customer', 'status']
 
 
     def validate(self, data):
-        # Custom validation logic
-        
-        if data['pickup_datetime'] < timezone.now():
+        if data['pickup_datetime'] < now():
             raise serializers.ValidationError({"pickup_datetime": "Pickup time must be in the future."})
         return data
-    
-    def create(self, validated_data):
-        # Set default status and the customer from the request context
-        validated_data['customer'] = self.context['request'].user
-        validated_data['status'] = 'requested'  # Assuming 'requested' is the initial status for new rides
-        return super().create(validated_data)
 
+
+    def create(self, validated_data):
+        validated_data['customer'] = self.context['request'].user
+        return super().create(validated_data)
--- uber_transport_simulation/backend/rides/views.py
+++ uber_transport_simulation/backend/rides/views.py
@@ -1,65 +1,3 @@
-# from rest_framework import viewsets, status
-# from rest_framework.response import Response
-# from rest_framework.decorators import action
-# from rides.models import Ride
-# from .serializers import RideSerializer
-# from django.db.models import Count
-# from rest_framework.permissions import IsAuthenticated
-
-# class RideViewSet(viewsets.ModelViewSet):
-#     queryset = Ride.objects.all()
-#     serializer_class = RideSerializer
-#     permission_classes = [IsAuthenticated]
-
-#     def create(self, request, *args, **kwargs):
-#         print("Incoming Data:", request.data)
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         response_data = {
-#         "message": "Ride created successfully",
-#         "ride": serializer.data  # Include the serialized ride data
-#     }
-#         return Response(response_data, status=status.HTTP_201_CREATED)
-
-#     def update(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(instance, data=request.data, partial=True)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-#         response_data = {
-#         "message": "Ride Updated successfully",
-#         "ride": serializer.data  # Include the serialized ride data
-#     }
-#         return Response(response_data)
-
-#     def destroy(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         self.perform_destroy(instance)
-#         return Response({"message": "Ride deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-
-#     @action(detail=False, methods=['get'], url_path='customer/(?P<customer_id>[^/.]+)')
-#     def list_customer_rides(self, request, customer_id=None):
-#         rides = Ride.objects.filter(customer__customer_id=customer_id)
-#         serializer = self.get_serializer(rides, many=True)
-#         return Response(serializer.data)
-
-#     @action(detail=False, methods=['get'], url_path='driver/(?P<driver_id>[^/.]+)')
-#     def list_driver_rides(self, request, driver_id=None):
-#         rides = Ride.objects.filter(driver__driver_id=driver_id)
-#         serializer = self.get_serializer(rides, many=True)
-#         return Response(serializer.data)
-
-#     @action(detail=False, methods=['get'], url_path='statistics/location')
-#     def ride_statistics(self, request):
-#         location_stats = Ride.objects.values('pickup_location').annotate(total_rides=Count('id'))
-#         return Response(location_stats)
-    
-#     def perform_create(self, serializer):
-#         # Serializer handles setting the customer and status
-#         serializer.save(customer=self.request.user)
-
-
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -70,10 +8,13 @@ from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
 
+
+
 class RideViewSet(viewsets.ModelViewSet):
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
     permission_classes = [IsAuthenticated]
+
 
     def create(self, request, *args, **kwargs):
         print("Incoming Data:", request.data)
@@ -86,6 +27,7 @@ class RideViewSet(viewsets.ModelViewSet):
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
 
+
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
@@ -97,10 +39,12 @@ class RideViewSet(viewsets.ModelViewSet):
         }
         return Response(response_data)
 
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({"message": "Ride deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 
     @action(detail=False, methods=['get'], url_path='customer/(?P<customer_id>[^/.]+)')
     def list_customer_rides(self, request, customer_id=None):
@@ -108,16 +52,19 @@ class RideViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(rides, many=True)
         return Response(serializer.data)
 
+
     @action(detail=False, methods=['get'], url_path='driver/(?P<driver_id>[^/.]+)')
     def list_driver_rides(self, request, driver_id=None):
         rides = Ride.objects.filter(driver__id=driver_id)
         serializer = self.get_serializer(rides, many=True)
         return Response(serializer.data)
 
+
     @action(detail=False, methods=['get'], url_path='statistics/location')
     def ride_statistics(self, request):
         location_stats = Ride.objects.values('pickup_location').annotate(total_rides=Count('id'))
         return Response(location_stats)
+
 
     @action(detail=True, methods=['patch'], url_path='status')
     def update_status(self, request, pk=None):
@@ -129,9 +76,11 @@ class RideViewSet(viewsets.ModelViewSet):
         ride.save()
         return Response({"message": "Ride status updated successfully", "status": ride.status})
 
+
     def perform_create(self, serializer):
         # Automatically set the customer to the logged-in user and default status to "requested"
         serializer.save(customer=self.request.user)
+
 
     def perform_update(self, serializer):
         serializer.save()
