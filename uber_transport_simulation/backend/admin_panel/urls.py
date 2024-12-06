ccd9065eeb1bae895706d0b0b704564e900e5d1a
“Charishma-Cherry” <charishmatamarana@gmail.com>
2024-12-05 20:49:12 -0800

    Admin with Manage users , Manage Drivers , Manage rides

--- uber_transport_simulation/backend/admin_panel/urls.py
+++ uber_transport_simulation/backend/admin_panel/urls.py
@@ -6,9 +6,22 @@ router = DefaultRouter()
 router.register(r'admins', AdminViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('manage_users/', AdminViewSet.as_view({'get': 'manage_users'})),
-    path('create_user/', AdminViewSet.as_view({'post': 'create_user'})),
-    path('update_user/', AdminViewSet.as_view({'put': 'update_user'})),
-    path('delete_user/', AdminViewSet.as_view({'delete': 'delete_user'})),
+    path('', include(router.urls)),  
+    #Customer Management
+    path('admins/list_customers/', AdminViewSet.as_view({'get': 'list_customers'}), name='list-customers'),
+    path('admins/add_customer/', AdminViewSet.as_view({'post': 'add_customer'}), name='add-customer'),
+    path('admins/update_customer/<str:pk>/', AdminViewSet.as_view({'put': 'update_customer'}), name='update-customer'),
+    path('admins/delete_customer/<str:pk>/', AdminViewSet.as_view({'delete': 'delete_customer'}), name='delete-customer'),
+    #Driver Management
+    path('admins/list_drivers/', AdminViewSet.as_view({'get': 'list_drivers'}), name='list-drivers'),
+    path('admins/add_driver/', AdminViewSet.as_view({'post': 'add_driver'}), name='add-driver'),
+    path('admins/update_driver/<str:pk>/', AdminViewSet.as_view({'put': 'update_driver'}), name='update-driver'),
+    path('admins/delete_driver/<str:pk>/', AdminViewSet.as_view({'delete': 'delete_driver'}), name='delete-driver'),
+    #Rides Management
+    path('admins/list_rides/', AdminViewSet.as_view({'get': 'list_rides'}), name='list-rides'),
+    # path('admins/review_ride/<str:pk>/', AdminViewSet.as_view({'get': 'review_ride'}), name='review-ride'),
+    path('admins/update_ride/<str:pk>/', AdminViewSet.as_view({'put': 'update_ride'}), name='update-ride'),
+    path('admins/delete_ride/<str:pk>/', AdminViewSet.as_view({'delete': 'delete_ride'}), name='delete-ride'),
 ]
+
+
--- uber_transport_simulation/frontend/uber_frontend/src/App.js
+++ uber_transport_simulation/frontend/uber_frontend/src/App.js
@@ -21,6 +21,8 @@ import AdminDashboard from './components/Admin/AdminDashboard';
 import AdminProfile from './components/Admin/AdminProfile';
 import AdminPrivateRoute from './components/Admin/AdminPrivateRoute';
 import ManageUsers from './components/Admin/ManageUsers';
+import ManageDrivers from './components/Admin/ManageDrivers';
+import ManageRides from './components/Admin/ManageRides';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RideHistory from './components/Rides/RideHistory';
 
@@ -79,6 +81,18 @@ function App() {
               <ManageUsers />  
             </AdminPrivateRoute>
           } />
+          <Route path="/admin/manage-drivers" element={
+            <AdminPrivateRoute>
+              <ManageDrivers />  
+            </AdminPrivateRoute>
+          } />
+          <Route path="/admin/manage-rides" element={
+            <AdminPrivateRoute>
+              <ManageRides />  
+            </AdminPrivateRoute>
+          } />
+
+
         <Route
             path="/user/ride-history" // Added RideHistory route
             element={
