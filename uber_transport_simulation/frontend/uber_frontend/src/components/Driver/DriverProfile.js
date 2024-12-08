48baefe8df5d0b386a3a4814249e88ac875fb5eb
Shobhita <aggrawalshobhi24@gmail.com>
2024-12-07 19:39:31 -0800

    Adding rating to driver profile

--- uber_transport_simulation/frontend/uber_frontend/src/components/Driver/DriverProfile.js
+++ uber_transport_simulation/frontend/uber_frontend/src/components/Driver/DriverProfile.js
@@ -218,6 +218,7 @@ function DriverProfile() {
                         <p><strong>Address:</strong> {driverData.address}, {driverData.city}, {driverData.state}, {driverData.zip_code}</p>
                         <p><strong>Car:</strong> {driverData.car_name} - {driverData.car_number}</p>
                         <p><strong>Phone:</strong> {driverData.phone_number}</p>
+                        <p><strong>My ratings:</strong> {driverData.rating} ({driverData.num_ratings} reviews)</p>
                         <div className="button-container">
                             <button onClick={() => setIsEditing(true)}>Edit Profile</button>
                             <button onClick={() => navigate(`/driver/${driverId}/select-location`)}> Select Location</button>
